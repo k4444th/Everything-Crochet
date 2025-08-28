b18f99399f0bc5e613c97095ddaa673e722771be
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-08-28 10:24:41 +0200

    Initial Commit

---
+++ Everything Crochet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Everything Crochet
+//
+//  Created by Katharina Weinberger on 28.08.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Everything Crochet/Everything_CrochetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Everything_CrochetApp.swift
+//  Everything Crochet
+//
+//  Created by Katharina Weinberger on 28.08.25.
+//
+
+import SwiftUI
+
+@main
+struct Everything_CrochetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
