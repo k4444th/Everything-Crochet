129f7307f94d23c1d5f518ed7475be0145c792ba
Katharina Weinberger <katharina.weinberger@student.tugraz.at>
2025-09-02 14:30:17 +0200

    Make projectDetails a little prettier

--- Everything Crochet/Datatypes/Pattern.swift
+++ Everything Crochet/Datatypes/Pattern.swift
@@ -3,6 +3,9 @@ import SwiftUI
 struct Pattern : Identifiable, Codable, Equatable {
     var id: Int = -1
     var name: String = ""
+    var previewImage: Data?
+    var tags: [String] = []
+    var techniques: String = ""
     var notes: String = ""
     var pattern: Data?
 }
--- Everything Crochet/ProjectDetail/NormalMode/DetailsView.swift
+++ Everything Crochet/ProjectDetail/NormalMode/DetailsView.swift
@@ -18,50 +18,91 @@ struct DetailsView: View {
             ]
             
             LazyVGrid(columns: columns, alignment: .leading) {
-                Image(systemName: "hammer")
+                VStack {
+                    Image(systemName: "hammer")
                         .imageScale(.large)
-                        .foregroundColor(Color.accent).padding(.bottom, 8)
-                
-                HStack {
-                    Text("Techniques:").padding(.bottom, 8)
-                    Text(techniques).padding(.bottom, 8)
+                        .foregroundColor(Color.accent)
                     Spacer()
                 }
                 
-                Image(systemName: "scissors")
-                        .imageScale(.large)
-                        .foregroundColor(Color.accent).padding(.bottom, 8)
-                
                 HStack {
-                    Text("Yarn:").padding(.bottom, 8)
-                    Text(yarn).padding(.bottom, 8)
+                    VStack {
+                        Text("Techniques:").padding(.bottom, 8)
+                        Spacer()
+                    }
+                    VStack {
+                        Text(techniques).padding(.bottom, 8)
+                        Spacer()
+                    }
                     Spacer()
                 }
                 
-                Image(systemName: "clock")
+                VStack {
+                    Image(systemName: "scissors")
                         .imageScale(.large)
-                        .foregroundColor(Color.accent).padding(.bottom, 8)
+                        .foregroundColor(Color.accent)
+                    Spacer()
+                }
+                HStack {
+                    VStack {
+                        Text("Yarn:").padding(.bottom, 8)
+                        Spacer()
+                    }
+                    VStack {
+                        Text(yarn).padding(.bottom, 8)
+                        Spacer()
+                    }
+                    Spacer()
+                }
+                
+                VStack {
+                    Image(systemName: "clock")
+                        .imageScale(.large)
+                        .foregroundColor(Color.accent)
+                    Spacer()
+                }
                 VStack {
                     HStack {
-                        Text("Startdate:").padding(.bottom, 8)
-                        Text(startdate).padding(.bottom, 8)
+                        VStack {
+                            Text("Startdate:").padding(.bottom, 8)
+                            Spacer()
+                        }
+                        VStack {
+                            Text(startdate).padding(.bottom, 8)
+                            Spacer()
+                        }
                         Spacer()
                     }
                     HStack {
-                        Text("Enddate:").padding(.bottom, 8)
-                        Text(enddate).padding(.bottom, 8)
+                        VStack {
+                            Text("Enddate:").padding(.bottom, 8)
+                            Spacer()
+                        }
+                        VStack {
+                            Text(enddate).padding(.bottom, 8)
+                            Spacer()
+                        }
                         Spacer()
                     }
                 }
                
-                
-                Image(systemName: "calendar")
-                    .imageScale(.large)
-                    .foregroundColor(Color.accent).padding(.bottom, 8)
+               
+                VStack {
+                    Image(systemName: "calendar")
+                        .imageScale(.large)
+                        .foregroundColor(Color.accent)
+                    Spacer()
+                }
                 
                 HStack {
-                    Text("Deadline:").padding(.bottom, 8)
-                    Text(deadline).padding(.bottom, 8)
+                    VStack {
+                        Text("Deadline:").padding(.bottom, 8)
+                        Spacer()
+                    }
+                    VStack {
+                        Text(deadline).padding(.bottom, 8)
+                        Spacer()
+                    }
                     Spacer()
                 }
             }
@@ -71,5 +112,5 @@ struct DetailsView: View {
 
 
 #Preview {
-    DetailsView(techniques: .constant("Tunesian Crochet"), startdate: .constant("21.05.2025"), enddate: .constant("-"), deadline: .constant("15.3.2026"), yarn: .constant("Gründl Lisa Premium"))
+    DetailsView(techniques: .constant("Tunesian Crochet, TSS, Join-as-you-go-Methode"), startdate: .constant("21.05.2025"), enddate: .constant("-"), deadline: .constant("15.3.2026"), yarn: .constant("Gründl Lisa Premium"))
 }
